e83295d2b093e8054c803bfeb096725c2367182a
iCCH27 <islam.gerland.27@gmail.com>
2020-11-28 13:09:09 +0100

    no css

--- register.php
+++ register.php
@@ -81,13 +81,13 @@ try {
     $mail->isSMTP();                                            // Send using SMTP
     $mail->Host       = 'smtp.gmail.com';
     $mail->SMTPAuth   = true;  // Enable SMTP authentication
-    $mail->Username   = 'bibliodzmail@gmail.com'; 
-    $mail->Password   = "BiblioDZ27000";
+    $mail->Username   = '@gmail.com'; 
+    $mail->Password   = "";
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
     //Recipients
-    $mail->setFrom('bibliodzmail@gmail.com', 'Register login tutorial');
+    $mail->setFrom('@gmail.com', 'Register login tutorial');
     $mail->addAddress($_POST['email'], $_POST['username']);     // Add a recipient
 
     // Content
